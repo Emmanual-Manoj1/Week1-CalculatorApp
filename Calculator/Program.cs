ec03350872b3f06db4bff0e1d6131047c6a0d861
Emmanual-Manoj1 <manoj-e@ulster.ac.uk>
2025-09-29 15:53:17 +0100

    Updated comments

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,17 @@
-﻿Console.WriteLine("Enter the first number: ");
-int firstnumber = Convert.ToInt32(Console.ReadLine());
+﻿
+
+// prints the txt for user to enter the first number
+Console.WriteLine("Enter the first number: ");
+// takes first number as input from the user
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// prints the txt for user to enter the second number
 Console.WriteLine("Enter the second number: ");
-int secondnumber = Convert.ToInt32(Console.ReadLine());
+// takes second number as input from the user
+int secondNumber = Convert.ToInt32(Console.ReadLine()); 
 
-int result = firstnumber + secondnumber;    
+//Adds the both number
+int result = firstNumber + secondNumber;    
 
-Console.WriteLine("The Sum of {0} and {1} is {2}",firstnumber, secondnumber, result);
+//prints out the result 
+Console.WriteLine("The Sum of {0} and {1} is {2}",firstNumber, secondNumber, result); 
