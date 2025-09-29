b426333d793c9536886a6639c17d102594c30df8
Emmanual-Manoj1 <manoj-e@ulster.ac.uk>
2025-09-29 15:40:28 +0100

    Updated program.cs file..

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the first number: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the second number: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstnumber + secondnumber;    
+
+Console.WriteLine("The Sum is : " + result);
